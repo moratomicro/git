94b5ec6e8b8c648bff65f04fc25449021dc9d59d
moratomicro <moratomicro@yahoo.com.br>
2021-08-16 10:57:36 -0300

    Adicionado novo gato

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,10 +7,12 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		
 		Gato gato = new Gato("Miau", "Preto e Branco", 2);
+		Gato gato2 = new Gato("Peste", "Malhado", 1);
 		
 		Livros livros = new Livros();
 		
 		System.out.println(gato);
+		System.out.println(gato2);
 		System.out.println(livros);
 		
 		/*int a = 2;
