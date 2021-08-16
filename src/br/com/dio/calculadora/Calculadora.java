c2e8da31010e75cb5f38dcfd15972382cb83acd1
moratomicro <moratomicro@yahoo.com.br>
2021-08-16 11:42:53 -0300

    calculadora

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,43 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+import java.util.function.ToDoubleFunction;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		int multiplicacao = multiplicacao(a,b);
+		int divisao = divisao(a, b);
+		
+		System.out.println("Soma " + a + " + " + b + " = " + soma);
+		System.out.println("Subtração " + a + " - " + b + " = " + subtracao);
+		System.out.println("Multiplicação " + a + " * " + b + " = "  + multiplicacao);
+		System.out.println("Divisão " + a + " / " + b + " = "  + divisao);
+	}
+	
+	public static int  soma(int a, int b) {
+		return a + b;
+	}
+	
+	public static int  subtracao(int a, int b) {
+		return a - b;
+	}
+	public static int  divisao(int a, int b) {
+		return a / b;
+	}
+	public static int  multiplicacao(int a, int b) {
+		return a * b;
+	}
+
+}
